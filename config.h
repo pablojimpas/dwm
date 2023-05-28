9fe8d38385d1fb2025cb483aa76ae73b2b0b63e0
Pablo Jiménez Pascual <pablo@jimpas.me>
2023-05-28 17:54:03 +0200

    Use Go font

--- config.h
+++ config.h
@@ -17,7 +17,7 @@ static int swallowfloating    = 0;	/* 1 means swallow floating windows by defaul
 static int smartgaps          = 1;	/* 1 means no outer gap when there is only one window */
 static int showbar            = 1;	/* 0 means no bar */
 static int topbar             = 1;	/* 0 means bottom bar */
-static char *fonts[]          = { "monospace:size=12", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
+static char *fonts[]          = { "Go:size=12:antialias=true:autohint=true", "monospace:size=12", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
 static char normbgcolor[]           = "#1d2021";
 static char normbordercolor[]       = "#1d2021";
 static char normfgcolor[]           = "#ebdbb2";

4e16536b2dd31fb5bbc3c08bfda10ab0bbdb93c3
Pablo Jiménez Pascual <pablo@jimpas.me>
2024-01-09 10:19:50 +0100

    Use sans font in status bar

--- config.h
+++ config.h
@@ -17,7 +17,7 @@ static int swallowfloating    = 0;	/* 1 means swallow floating windows by defaul
 static int smartgaps          = 1;	/* 1 means no outer gap when there is only one window */
 static int showbar            = 1;	/* 0 means no bar */
 static int topbar             = 1;	/* 0 means bottom bar */
-static char *fonts[]          = { "Go:size=12:antialias=true:autohint=true", "monospace:size=12", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
+static char *fonts[]          = { "sans:size=12", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
 static char normbgcolor[]           = "#1e1e2e";
 static char normbordercolor[]       = "#1e1e2e";
 static char normfgcolor[]           = "#cdd6f4";

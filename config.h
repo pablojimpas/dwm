6d033311ebe6b4ca7654a20d6538499c7b065352
Pablo Jiménez Pascual <pablo@jimpas.me>
2024-03-13 22:29:39 +0100

    Change colors back to gruvbox

--- config.h
+++ config.h
@@ -18,12 +18,12 @@ static int smartgaps          = 1;	/* 1 means no outer gap when there is only on
 static int showbar            = 1;	/* 0 means no bar */
 static int topbar             = 1;	/* 0 means bottom bar */
 static char *fonts[]          = { "sans:size=12", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
-static char normbgcolor[]           = "#1e1e2e";
-static char normbordercolor[]       = "#1e1e2e";
-static char normfgcolor[]           = "#cdd6f4";
-static char selfgcolor[]            = "#1e1e2e";
-static char selbordercolor[]        = "#74c7ec";
-static char selbgcolor[]            = "#a6e3a1";
+static char normbgcolor[]           = "#1d2021";
+static char normbordercolor[]       = "#1d2021";
+static char normfgcolor[]           = "#ebdbb2";
+static char selfgcolor[]            = "#fbf1c7";
+static char selbordercolor[]        = "#fb4934";
+static char selbgcolor[]            = "#458588";
 static char *colors[][3] = {
        /*               fg           bg           border   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
